5a00a4b84a9f99ccfe9769293559df55c3ad8af5
michal <michwey@gmail.com>
2024-03-21 20:59:47 +0100

    convoluted

--- src/exercise/01_backup.js
+++ src/exercise/01_backup.js
@@ -77,14 +77,6 @@ function useUser() {
 // Then go down to the `handleSubmit` from `UserSettings` and put that logic in
 // this function. It should accept: dispatch, user, and updates
 
-function updateUser(dispatch, user, updates) {
-  dispatch({type: 'start update', updates: updates});
-  userClient.updateUser(user, updates).then(
-    updatedUser => dispatch({type: 'finish update', updatedUser}),
-    error => dispatch({type: 'fail update', error}),
-  )
-}
-
 // export {UserProvider, useUser}
 
 // src/screens/user-profile.js
@@ -105,7 +97,12 @@ function UserSettings() {
 
   function handleSubmit(event) {
     event.preventDefault()
-    updateUser(userDispatch, user, formState);
+    // 🐨 move the following logic to the `updateUser` function you create above
+    userDispatch({type: 'start update', updates: formState})
+    userClient.updateUser(user, formState).then(
+      updatedUser => userDispatch({type: 'finish update', updatedUser}),
+      error => userDispatch({type: 'fail update', error}),
+    )
   }
 
   return (
