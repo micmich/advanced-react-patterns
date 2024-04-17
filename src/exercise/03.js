daaa3cbbde77068449ec26adf7ff73376fee674b
michal <michwey@gmail.com>
2024-04-17 19:41:27 +0200

    error handling draft

--- src/exercise/03.js
+++ src/exercise/03.js
@@ -9,13 +9,21 @@ import {Switch} from '../switch'
 
 const ToggleContext = React.createContext({});
 
+function useToggle() {
+  const { on, toggle } = React.useContext(ToggleContext);
+  if (typeof on === 'undefined') {
+    throw new Error("Missing ToggleContext - wrap component in <Toggle /> parent.")
+  }
+  return {on, toggle}
+}
+
 function Toggle({children}) {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
 
   // 🐨 remove all this 💣 and instead return <ToggleContext.Provider> where
   // the value is an object that has `on` and `toggle` on it.
-  return <ToggleContext.Provider value={[on, toggle]} >{children}</ToggleContext.Provider>
+  return <ToggleContext.Provider value={{on, toggle}} >{children}</ToggleContext.Provider>
 }
 
 // 🐨 we'll still get the children from props (as it's passed to us by the
@@ -26,33 +34,21 @@ function Toggle({children}) {
 // 💰 `const context = React.useContext(ToggleContext)`
 // 📜 https://react.dev/reference/react/useContext
 function ToggleOn({children}) {
-  return React.useContext(ToggleContext)[0] ? children : null
+  return useToggle().on ? children : null
 }
 
 // 🐨 do the same thing to this that you did to the ToggleOn component
 function ToggleOff({children}) {
-  return React.useContext(ToggleContext)[0] ? null : children
+  return useToggle().on ? null : children
 }
 
 // 🐨 get `on` and `toggle` from the ToggleContext with `useContext`
 function ToggleButton({...props}) {
-  const [on, toggle] = React.useContext(ToggleContext);
+  const {on, toggle} = useToggle();
   return <Switch on={on} onClick={toggle} {...props} />
 }
 
-function App() {
-  return (
-    <div>
-      <Toggle>
-        <ToggleOn>The button is on</ToggleOn>
-        <ToggleOff>The button is off</ToggleOff>
-        <div>
-          <ToggleButton />
-        </div>
-      </Toggle>
-    </div>
-  )
-}
+const App = () => <ToggleButton />
 
 export default App
 
